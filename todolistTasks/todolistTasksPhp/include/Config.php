6a968b8a3b9d879da47a91b56d7906b4d09f0de3
shaiGettu <shaige2@ac.sce.ac.il>
2017-05-06 13:16:13 +0300

    Add files via upload

---
+++ todolistTasks/todolistTasksPhp/include/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shai Gettu
+ * Date: 22-Apr-17
+ * Time: 3:56 PM
+ */
+/**
+ * Database config variables
+ */
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASSWORD","");
+define("DB_DATABASE","widgets");
+?>
+
