7a81475c70ee3c8fd1a2a548f284fe1819d6b54b
rotemsi1 <rotemsinger2011@gmail.com>
2017-05-20 22:32:38 +0300

    The widgets together in one application including login

---
+++ Widgets/src/Login/Login.java
@@ -0,0 +1,15 @@
+package Login;
+
+public class Login {
+	
+	private int index;
+	private String email;
+	private String password;
+	
+	public Login(String email, String password) {
+		index = 1;
+		this.email = email;
+		this.password = password;
+	}
+	
+}
